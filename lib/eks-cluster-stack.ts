9ab567acd41c78387f15a3048c24770341f784df
Shawn Zhang <hustshawn@gmail.com>
2026-02-23 08:33:51 +0000

    Add EKS cluster stack with managed node group

---
+++ lib/eks-cluster-stack.ts
@@ -0,0 +1,62 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as eks from 'aws-cdk-lib/aws-eks';
+import { KubectlV31Layer } from '@aws-cdk/lambda-layer-kubectl-v31';
+import { Construct } from 'constructs';
+
+export interface EksClusterStackProps extends cdk.StackProps {
+  vpc: ec2.IVpc;
+}
+
+export class EksClusterStack extends cdk.Stack {
+  public readonly cluster: eks.Cluster;
+
+  constructor(scope: Construct, id: string, props: EksClusterStackProps) {
+    super(scope, id, props);
+
+    const { vpc } = props;
+
+    // Create EKS cluster
+    this.cluster = new eks.Cluster(this, 'EksCluster', {
+      vpc,
+      version: eks.KubernetesVersion.V1_31,
+      kubectlLayer: new KubectlV31Layer(this, 'kubectl'),
+      defaultCapacity: 0,
+      endpointAccess: eks.EndpointAccess.PUBLIC_AND_PRIVATE,
+      vpcSubnets: [{ subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS }],
+      clusterName: 'eks-blueprints-cluster',
+    });
+
+    // Add managed node group
+    this.cluster.addNodegroupCapacity('ManagedNodeGroup', {
+      instanceTypes: [new ec2.InstanceType('t3.medium')],
+      minSize: 1,
+      maxSize: 3,
+      desiredSize: 2,
+      amiType: eks.NodegroupAmiType.AL2023_X86_64_STANDARD,
+      diskSize: 100,
+    });
+
+    // Outputs
+    new cdk.CfnOutput(this, 'ClusterName', {
+      value: this.cluster.clusterName,
+      description: 'EKS Cluster Name',
+      exportName: 'EksClusterName',
+    });
+
+    new cdk.CfnOutput(this, 'ClusterArn', {
+      value: this.cluster.clusterArn,
+      description: 'EKS Cluster ARN',
+    });
+
+    new cdk.CfnOutput(this, 'ClusterEndpoint', {
+      value: this.cluster.clusterEndpoint,
+      description: 'EKS Cluster Endpoint',
+    });
+
+    new cdk.CfnOutput(this, 'KubectlRoleArn', {
+      value: this.cluster.kubectlRole?.roleArn ?? 'N/A',
+      description: 'kubectl Role ARN',
+    });
+  }
+}
